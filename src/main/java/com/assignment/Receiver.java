0ca00efed729081dc47951d146e7ef1a78dbbb9d
BillyEsteves <joaopalma.esteves@gmail.com>
2026-02-27 18:10:47 +0000

    Created the project

---
+++ src/main/java/com/assignment/Receiver.java
@@ -0,0 +1,12 @@
+package com.assignment;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Receiver {
+    
+    public static void main(String[] args) throws Exception {
+
+    }
+}
---
+++ src/main/java/com/assignment/Sender.java
@@ -0,0 +1,11 @@
+package com.assignment;
+
+import java.io.*;
+import java.net.Socket;
+
+public class Sender {
+    
+    public static void main(String[] args) throws Exception {
+
+    }
+}
